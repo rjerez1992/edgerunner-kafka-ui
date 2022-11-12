9dc6c3596016a94d9a69f025dc9f5bf011b56435
Reinaldo Jerez Contreras <rjerez1992@gmail.com>
2022-11-12 01:03:54 -0300

    Add $RAND[]$ data generator. Add _STR generators for number related ones.

--- src/app/utils/data-generator.ts
+++ src/app/utils/data-generator.ts
@@ -4,6 +4,8 @@ import { faker } from '@faker-js/faker'
 //https://fakerjs.dev/api/
 export class DataGenerator {
 
+  private static randRegex : RegExp = /\"\$RAND\[[a-zA-Z0-9,\\"]*\]\$\"/gm;
+
   public static processString(input: string): string {
     console.log("Data generator processing input");
     console.log(input);
@@ -14,6 +16,9 @@ export class DataGenerator {
     input = this.replaceTokens(input, "\"$NUMBER$\"", faker.datatype.number);
     input = this.replaceTokens(input, "\"$BIG_NUMBER$\”", faker.datatype.bigInt);
     input = this.replaceTokens(input, "\"$FLOAT$\"", faker.datatype.float);
+    input = this.replaceTokens(input, "$NUMBER_STR$", faker.datatype.number);
+    input = this.replaceTokens(input, "$BIG_NUMBER_STR$", faker.datatype.bigInt);
+    input = this.replaceTokens(input, "$FLOAT_STR$", faker.datatype.float);
     input = this.replaceTokens(input, "$DATE$", faker.datatype.datetime);
 
     //Names
@@ -23,6 +28,7 @@ export class DataGenerator {
 
     //Finnance
     input = this.replaceTokens(input, "\"$AMOUNT$\"", DataGenerator.integerAmount);
+    input = this.replaceTokens(input, "$AMOUNT_STR$", DataGenerator.integerAmount);
     input = this.replaceTokens(input, "$CARD_NUMBER$", faker.finance.creditCardNumber);
     input = this.replaceTokens(input, "$ACCOUNT_NUMBER$", faker.finance.account);
 
@@ -39,12 +45,49 @@ export class DataGenerator {
 
     //Lorem
     input = this.replaceTokens(input, "$SENTENCE$", faker.lorem.sentence);
+
+    //Rand
+    input = this.processRand(input);
   
     console.log("Data generator processing output");
     console.log(input);
 
     return input;
   }  
+
+  private static processRand(input: string): string {
+    console.log("Processing RAND token");
+    const randEntries = input.match(DataGenerator.randRegex);
+    console.log(randEntries);
+    
+    if (randEntries != undefined){
+      
+      for (let i = 0; i < randEntries.length; i++) {
+        var token = randEntries[i];
+        var strValues = randEntries[i];
+        console.log(strValues);
+        strValues = strValues.replace('"$RAND[', "");
+        strValues = strValues.replace(']$"', "");
+        console.log(strValues);
+        var values = strValues.split(",");
+       
+        if (values.length > 0){
+          var selected = values[Math.floor(Math.random()*values.length)];
+          while (selected.includes('\\"')){
+            selected = selected.replace('\\"', '"');
+          }
+          input = input.replace(token, selected);
+        }
+      }
+    }
+
+    return input;
+  }
+
+  /*private static isNumeric(n: string) {
+    let flt = parseFloat(n);
+    return !isNaN(flt) && isFinite(flt);
+  }*/
 
   private static integerAmount(): string {
     return faker.finance.amount(100, 1000000, 0);
